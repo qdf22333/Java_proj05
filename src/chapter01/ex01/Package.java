2cd7b46606b9f1780df8f94ceab885ecb0847b1b
yerim park <qdf22333@hanmail.net>
2022-11-09 11:38:38 +0900

    첫번재 커밋

---
+++ src/chapter01/ex01/Package.java
@@ -0,0 +1,18 @@
+package chapter01.ex01;
+
+/*
+ 패키지 (package) : 클래스 파일이 위치하는 폴더
+    -- 패키지 명을 넣지 않으면 default package에 class파일이 저장
+    -- 하위 패키지를 만들 때는 . 을 사용해서 하위 패키지를 생성
+    -- 동일한 이름의 클래스파일을 패키지로 분리해서 생성할 수 있다.
+    -- 외부의 클래스 파일을 가져올 때는 패키지로 분리해서 가져올 수 있다.
+    -- 
+*/
+public class Package {
+
+	public static void main(String[] args) {
+		System.out.println("패키지 출력");
+
+	}
+
+}
---
+++ src/chapter01/ex01/Test_class.java
@@ -0,0 +1,11 @@
+package chapter01.ex01;
+
+public class Test_class {
+	public static void main(String[] args) {
+		
+		// 패키지 생성, 클래스 생성, 클래스를 패키지 사이로 이동, 하위 패키지 생성
+		// 기본적으로 JSP나 Spring을 구성할 때 3단계의 Package가 기본적으로 등록
+		// com.naver.www : 클래스 저장
+		
+	}
+}
